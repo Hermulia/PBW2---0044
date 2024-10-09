d977daf3c7fd8a6d0889471406ab101820f929d6
Anjelita <hermuliaanjelita@gmail.com>
2024-10-09 17:17:00 +0700

    Modul 4 PBW 2

--- app/Http/Controllers/BukuController.php
+++ app/Http/Controllers/BukuController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Models\Buku;
-use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
+// use illuminate\Container\Attributes\DB;
+use Illuminate\Support\Facades\Storage;
+// use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreBukuRequest;
 use App\Http\Requests\UpdateBukuRequest;
 
@@ -14,7 +17,11 @@ class BukuController extends Controller
      */
     public function index()
     {
-        return view('buku/index');
+        return view('buku/index', [
+            'bukus' => DB::table('bukus')->get() 
+        // return view('buku/index', [
+        //     'bukus' => Buku::all() // Fetch data as Eloquent models
+    ]);
         //
     }
 
@@ -57,24 +64,53 @@ class BukuController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(Buku $buku)
+    public function edit($id)
     {
-        //
+    $test = DB::table('bukus')->where('id', $id)->get();
+    return view('buku/update',[
+        'buku' => $test[0]
+    ]);  //
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(UpdateBukuRequest $request, Buku $buku)
+    public function update(UpdateBukuRequest $request, $id)
     {
-        //
-    }
+        $validatedData = $request->validate([
+            
+            'judul' => 'required',
+            'penulis' => 'required',
+            'kategori' => 'required',
+            'sampul' => 'image|file|max:2048',
+        ]);
+        
+        if ($request->file('sampul')) {
+            if ($request->sampulLama) {
+                Storage::delete($request->sampulLama);
+            }
+            $validatedData['sampul'] = $request->file('sampul')->store('/sampul-buku');
+        }
+        
+        Buku::where('id', $id)->update($validatedData);
+        return redirect('/buku');
+    }   
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Buku $buku)
+    public function destroy($id)
     {
+    $test = DB::table('bukus')->select('sampul')
+        ->where('id', $id)
+        ->get();
+
+    if ($test[0]->sampul) {
+    Storage::delete($test[0]->sampul);
+    }
+
+    Buku::destroy($id);
+    return redirect('/buku');
         //
     }
 }
